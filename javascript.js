297f8a75171ce1b46ff213a3b4aeb4f316e28342
Agostina Contreras <agoscontreras1@gmail.com>
2023-05-11 20:22:38 -0300

    finish

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//punto numero 1
+let usuarioNombre = "Matias Rodriguez";
+let profesor = "Desarrollador";
+let edad = 25;
+
+console.log(` Hola soy ${usuarioNombre}, de apellido ${profesor} y tengo ${edad}`);
+
+//punto numero 2
+var años = prompt("Indique la edad de Matias")
+
+switch(años){
+    case "25":
+    console.log("“El usuario tiene 25 años");
+    break;
+    
+
+    default:
+    console.log("“El usuario no coincide con la edad ingresada");
+    }
+
+
+    //punto numero 4
+
+    var chocolate = 100;
+    var iva = 16;
+
+
+    let sumaNumeros = chocolate + iva
+    console.log(sumaNumeros);
